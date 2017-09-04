5de8fa3fa65705090cc83262375d9d4cf78c5d0e
christinechoi <smi77y@gmail.com>
2017-09-03 21:08:50 -0400

    Add purchase_date attribute to ItemSerializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :brand, :description, :cost, :collection_id, :user_id
+  attributes :id, :name, :brand, :description, :cost, :collection_id, :user_id, :purchase_date
   belongs_to :collection
 end
