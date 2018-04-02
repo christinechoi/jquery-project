d03a2287ede5bdf8ccdc9a85a9120408223273dc
christinechoi <smi77y@gmail.com>
2018-04-02 14:52:55 -0400

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170820004902) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "collections", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
@@ -45,8 +48,8 @@ ActiveRecord::Schema.define(version: 20170820004902) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
